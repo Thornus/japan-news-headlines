d183ebfdafdf8d198487f45a07ea42b1da5409fe
Thornus <lucapokecampione@gmail.com>
2024-09-16 18:55:58 -0400

    Use colorMode.preference instead of colorMode.value

--- components/ColorModeToggle.vue
+++ components/ColorModeToggle.vue
@@ -1,7 +1,7 @@
 <template>
   <ClientOnly>
     <button class="absolute right-1 bottom-1 p-2" @click="toggleColorMode">
-      <Icon v-if="colorMode.value === 'light'" name="heroicons:moon-solid" />
+      <Icon v-if="colorMode.preference === 'light'" name="heroicons:moon-solid" />
       <Icon v-else name="heroicons:sun-solid" />
     </button>
   </ClientOnly>
@@ -11,7 +11,7 @@
   const colorMode = useColorMode();
   
   const toggleColorMode = () => {
-    colorMode.value = colorMode.value === 'dark' ? 'light' : 'dark';
+    colorMode.preference = colorMode.preference === 'dark' ? 'light' : 'dark';
   };
 </script>
 
